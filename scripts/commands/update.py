5c26b1d380e90834c3df87a7b5f6052e426e6cc3
Tom Swirly <tom@swirly.com>
2011-06-20 17:36:33 -0400

    Shell of commands/update.py

--- scripts/commands/update.py
+++ scripts/commands/update.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
-#
+
 # Copyright (c) 2011 Tom Swirly
 #
 # This software is licensed under the terms of the artistic license,
@@ -7,11 +7,10 @@
 #
 # Essentially, you can do as you please with it.  Have fun!
 
-#
-# version is a juce command that just prints the version number!
-#
 
-VERSION = '0.2.1'
+# A Juce command that just prints the version number!
 
-def version():
-  return 'juce version %s' % VERSION
+def update(*packages):
+  if not packages:
+    raise Exception('No packages to update')
+  return 'Updated packages %s.' % ', '.join(packages)
